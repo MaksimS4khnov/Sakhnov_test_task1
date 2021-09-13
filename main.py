f85a42232d7717a998f81398c4fb022aaebea519
MaksimS4khnov <sahnoffmaksim@yandex.ru>
2021-09-13 22:43:03 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+
+def test_task(n):
+    #create empty array
+    start_arr = []
+
+    #add arrays to start_array
+    for i in range (0, n + 1):
+        #create transit_arr which we will fill in with random numbers
+        transit_arr = []
+        #create random length for array
+        transit_len = int((random.random())*100)
+        for j in range (0, transit_len + 1):
+            transit_arr.append(int((random.random())*100))
+
+        #sorting an array based on the following conditions
+        if ( i % 2 == 0 ):
+            transit_arr.sort()
+        else:
+            transit_arr.sort(reverse=True)
+
+        start_arr.append(transit_arr)
+
+    return (start_arr)
+
+
